3dac7c4fc280641ea822dd3989acd3b1e27fc1cf
Josep1992 <josep@eventray.com>
2021-07-14 00:41:40 -0400

    wip csv endpoint

--- csv/index.js
+++ csv/index.js
@@ -1,29 +1,39 @@
-module.exports = async function (context, req) {
+const Logger = require("../infra/logger");
+const { json2csvAsync } = require('json-2-csv');
 
-  logger.event("initialize", "downloading dataset");
+module.exports = async function (context, req) {
+  const { body } = req;
+  const logger = new Logger(context);
 
   let res = {
     status: 200,
-    body: {
-      type: 'csv',
-      file: 'here is a csv file',
-      error: null,
-    }
+    success: true,
+    error: null,
   };
 
   if (req.method === "POST") {
     try {
-      logger.event("request", "getting csv data");
+      if (!body.data || !Array.isArray(body.data)) {
+        throw new Error("body.data is required")
+      }
 
+      logger.event("request", "generating csv file");
+
+      const csv = await json2csvAsync(body.data)
+
+      logger.event("response", "sending back csv file");
+
+      context.res.header('Content-Type', 'text/csv');
+      return context.res.send(csv);
     } catch (error) {
       // do some error logging here
       logger.event("error", error.message);
 
-      res.body.error = error.message;
-      res.body.status = 500;
+      res.error = error.message;
+      res.success = false;
+      res.status = 500;
+      context.res = res;
     }
   }
-
-  logger.event("response", "sending back csv file");
-  context.res = res;
+  return context.res
 }
